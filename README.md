174ab402cd07100bedb66fc51a3024bff1a27b1d
PCiesielczyk <piotrciesielczyk2001@gmail.com>
2024-11-03 16:35:52 +0100

    doc: update README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # trello-dashboard
+
+## Prerequisites
+In `frontend/.env.local` add two fields to enable communication with Trello REST API:
+```
+NEXT_PUBLIC_TRELLO_API_KEY=<API_KEY>
+NEXT_PUBLIC_TRELLO_API_TOKEN=<API_TOKEN>
+```
--- frontend/hooks/useMetrics.ts
+++ frontend/hooks/useMetrics.ts
@@ -1,6 +1,6 @@
 import {fetchTrelloBoards} from "@/app/api/metrics/actions";
 import {useMutation} from "@tanstack/react-query";
-//TODO: update README, note in trello
+
 export const useTrelloBoards = () => {
     const { mutate: getBoards, data, isPending, isError, error } = useMutation({
         mutationFn: (username: string) => fetchTrelloBoards(username),
