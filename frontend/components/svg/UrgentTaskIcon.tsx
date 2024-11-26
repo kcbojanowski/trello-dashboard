01cede89e3ccf4f1fb54bf6f38e0e03b448c5077
kcbojanowski <k1bojanowski@gmail.com>
2024-11-26 18:28:16 +0100

    Add Icon components

---
+++ frontend/components/svg/UrgentTaskIcon.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const UrgentTaskIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className="h-4 w-4 text-muted-foreground"
+    >
+      <path
+        d="M8 16v-4a4 4 0 0 1 8 0v4M3 12h1m8-9v1m8 8h1M5.6 5.6l.7.7m12.1-.7l-.7.7M6 17a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1z" />
+    </svg>
+  )
+}
+
+export default UrgentTaskIcon
